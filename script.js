fe3207ab53866795c9ec5f288de98c308cf01ba1
Andrew Belcher <belcher.w.andrew@gmail.com>
2022-05-09 01:11:39 +0800

    Commit test tooltip

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ let padding = 100;
 
 let svg = d3.select('#heat-map')
                 .append('svg')
+                .attr('id', 'heat-map-svg');
+
+let tooltip = d3.select('#tooltip');                
 
 req.open('GET', url, true);
 req.send();
@@ -27,25 +30,32 @@ req.onload = () => {
     monthlyVarArr = json.monthlyVariance;
 
     // console.log(baseTemp);
-    console.log(monthlyVarArr);
+    // console.log(monthlyVarArr);
 
     // Generate scales
     // Draw cells
     // Draw axes
 
+    //Create min & max year variables
+    let minYear = d3.min(monthlyVarArr, (d) => d["year"]);
+    let maxYear = d3.max(monthlyVarArr, (d) => d["year"]);
+
     // Create an x and y scale
     xScale = d3.scaleLinear()
-                .domain([d3.min(monthlyVarArr, (d) => d["year"]), d3.max(monthlyVarArr, (d) => d["year"])])
+                .domain([minYear, maxYear + 1]) // Add 1 to max year to fit all data
                 .range([padding, width - padding])
 
     yScale = d3.scaleTime()
+                .domain([new Date(0, 0, 0, 0, 0, 0, 0), new Date(0, 12, 0, 0, 0, 0, 0)]) //#9 vertical position
                 // .domain([new Date(0, 11, 16), new Date(1, 11, 16)])
-                .domain([new Date(0, 0), new Date(0, 12)])
                 .range([padding, height - padding])
 
     // Add axes
-    let xAxis = d3.axisBottom(xScale);
-    let yAxis = d3.axisLeft(yScale);
+    let xAxis = d3.axisBottom(xScale)
+                    .tickFormat(d3.format("d"))
+
+    let yAxis = d3.axisLeft(yScale)
+                    .tickFormat(d3.timeFormat("%B")) // #11 yAxis ticks
 
     svg.append("g")
         .call(xAxis)
@@ -58,6 +68,7 @@ req.onload = () => {
         .attr("transform", "translate(" + padding + ", 0)");
 
     // #5 Add rect elements, #6 Add fill colours, #7/8 Add cell properties
+    // #9 Vertical position, #10 Horizontal position
     svg.selectAll("rect")
         .data(monthlyVarArr)
         .enter()
@@ -83,7 +94,46 @@ req.onload = () => {
         .attr("data-year", (d) => d.year)
         .attr("data-temp", (d) => baseTemp + d.variance)
         .attr("height", (height - padding * 2) / 12)
-        // .attr("y", (d) => yScale(new Date(0, d.month - 1)))
+        .attr("y", (d) => yScale(new Date(0, d.month - 1, 0, 0, 0, 0, 0)))
+        .attr("width", (width - padding * 2) / (maxYear - minYear))
+        .attr("x", (d) => xScale(d.year))
+        .on("mouseover", d => tooltip.text("Year: " + d.year))
+        .on("mouseout", tooltip.text("tooltip"))
+    
+    let legendArr = [
+        {color: "rgb(69, 117, 180)", text:"var < 1.5"},
+        {color: "rgb(171, 217, 233)", text:"-0.5 < var < -1.5"},
+        {color: "rgb(255, 255, 191)", text:"-0.5 < var < +0.5"},
+        {color: "rgb(253, 174, 97)", text:"+0.5 < var < +1.5"},
+        {color: "rgb(215, 48, 39)", text:"var > +1.5"}
+    ];
+    
+    let legend = d3.select("#legend")
+                    .append("svg")
+                    .attr("id", "legend-svg")
+
+    legend.selectAll("rect")
+            .data(legendArr)
+            .enter()
+            .append("rect")
+            .attr("fill", d => d.color)
+            .attr("height", 20)
+            .attr("y", 0)
+            .attr("width", 20)
+            .attr("x", (d, i) => i * 150 + 100)
+            
+    legend.selectAll("text")
+            .data(legendArr)
+            .enter()
+            .append("text")
+            .attr("fill", d => d.color)
+            .attr("y", 11.25)
+            .attr("x", (d, i) => i * 150 + 125)
+            .text(d => d.text)
+            .attr("text-anchor", "left")
+            .style("alignment-baseline", "middle")  
+
+                    
 }
 
 
