0f9dd87270bdbf9a15f63e0e166660b9af972399
Andrew Belcher <belcher.w.andrew@gmail.com>
2022-05-09 17:02:48 +0800

    Added tooltip

--- script.js
+++ script.js
@@ -65,12 +65,43 @@ req.onload = () => {
         .attr("id", "y-axis")
         .attr("transform", "translate(" + padding + ", 0)");
 
+    // #16 Tooltip
+    let tooltip = d3.select("#heat-map")
+                    .append("div")
+                    .attr("id", "tooltip")
+                    .style("opacity", 0)
+                    .attr("class", "tooltip")
+                    .style("background-color", "white")
+                    .style("border", "solid")
+                    .style("border-width", "2px")
+                    .style("border-radius", "5px")
+                    .style("padding", "5px");
+    var mouseover = function(d) {
+        tooltip.style("opacity", 1)
+        d3.select(this)
+            .style("stroke", "black")
+            .style("opacity", 1)
+    }
+    var mousemove = function(event, d) {
+        tooltip.attr("data-year", d.year)
+                .html(d.year)
+                .style("left", (d3.pointer(event)[0]) + "px")
+                .style("top", (d3.pointer(event)[1]) + "px")
+    }
+    var mouseleave = function(d) {
+        tooltip.style("opacity", 0)
+        d3.select(this)
+            .style("stroke", "none")
+            .style("opacity", 0.8)
+    }                
+
     // #5 Add rect elements, #6 Add fill colours, #7/8 Add cell properties
     // #9 Vertical position, #10 Horizontal position
     svg.selectAll("rect")
         .data(monthlyVarArr)
         .enter()
         .append("rect")
+        .attr("id", "data-point")
         .attr("class", "cell")
         .attr("fill", (d) => {
             if(d.variance < -1.5) {
@@ -95,6 +126,9 @@ req.onload = () => {
         .attr("y", (d) => yScale(new Date(0, d.month - 1, 0, 0, 0, 0, 0)))
         .attr("width", (width - padding * 2) / (maxYear - minYear))
         .attr("x", (d) => xScale(d.year))
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
     
     let legendArr = [
         {color: "rgb(69, 117, 180)", text:"var < 1.5"},
@@ -129,7 +163,71 @@ req.onload = () => {
             .attr("text-anchor", "left")
             .style("alignment-baseline", "middle")  
 
-                    
+
+    // Tooltip Testing
+
+    // let square = d3.select("#test")
+    // .append("svg")
+    // .attr("width", 1000)
+    // .attr("height", 400)
+
+    // let testData = [400, 500, 600];
+
+    // var testTooltip = d3.select("#test")
+    // .append("div")
+    // // .style("position", "absolute")
+    // // .style("visibility", "hidden")
+    // // .text("I'm a circle!")
+    // .style("opacity", 0)
+    // .attr("class", "tooltip")
+    // .style("background-color", "white")
+    // .style("border", "solid")
+    // .style("border-width", "2px")
+    // .style("border-radius", "5px")
+    // .style("padding", "5px")
+
+    // // Three function that change the tooltip when user hover / move / leave a cell
+    // var mouseoverTest = function(d) {
+    //     testTooltip
+    //     .style("opacity", 1)
+    //     d3.select(this)
+    //     .style("stroke", "black")
+    //     .style("opacity", 1)
+    // }
+
+    // var mousemoveTest = function(d) {
+    //     testTooltip
+    //     .text("The exact value of<br>this cell is: ")
+    //     .style("top", (1200)+"px")
+    //     .style("left",(475)+"px")
+    // }
+
+    // var mouseleaveTest = function(d) {
+    //     testTooltip
+    //     .style("opacity", 0)
+    //     d3.select(this)
+    //     .style("stroke", "none")
+    //     .style("opacity", 0.8)
+    // }
+
+    // square.selectAll("rect")
+    // .data(testData)
+    // .enter()
+    // .append("rect")
+    // .attr("id", "circleBasicTooltip")
+    // .attr("x", d => d)
+    // .attr("y", 200)
+    // .attr("width", 40)
+    // .attr("height", 40)
+    // .attr("fill", "#69b3a2")
+    // .on("mouseover", mouseoverTest)
+    // .on("mousemove", function(event, d) {
+    //     testTooltip
+    //     .text(d)
+    //     .style("left", (d3.pointer(event)[0]) + "px")
+    //     .style("top", (d3.pointer(event)[1] + 1000) + "px")
+    // })
+    // .on("mouseleave", mouseleaveTest)
 }
 
 
